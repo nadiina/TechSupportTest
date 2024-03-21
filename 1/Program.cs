af0d07fb322a0574d2f86383cad45ffccd359d00
nadina <nadiia.shcher@gmail.com>
2024-03-21 15:45:33 +0200

    add directions

---
+++ 1/Program.cs
@@ -0,0 +1,9 @@
+﻿public class HedgehogTest : HedgehogTestBase
+{
+    public static void Main(string[] args)
+    {
+        int[] hedgehogs = { 8, 1, 9 };
+        int desiredColor = 0; // Red
+        Console.WriteLine(MinEncounters(hedgehogs, desiredColor));
+    }
+}
---
+++ 1/functions/HedgehogTestBase.cs
@@ -0,0 +1,64 @@
+﻿public class HedgehogTestBase
+{
+    public static int MinEncounters(int[] hedgehogs, int desiredColor)
+    {
+        int moves = 0;
+        if (hedgehogs[0] == 0 && hedgehogs[1] == 0 && hedgehogs[2] == 0)
+        {
+            return -1;
+        }
+
+        if (hedgehogs[(desiredColor + 1) % 3] == hedgehogs[(desiredColor + 2) % 3])
+        {
+            moves += hedgehogs[(desiredColor + 1) % 3];
+            hedgehogs[(desiredColor + 1) % 3] -= hedgehogs[(desiredColor + 1) % 3];
+            hedgehogs[(desiredColor + 2) % 3] -= hedgehogs[(desiredColor + 2) % 3];
+        }
+        if (hedgehogs[(desiredColor + 1) % 3] == 0 || hedgehogs[(desiredColor + 2) % 3] == 0)
+        {
+            return -1;
+        }
+        moves += HandleColorComparison(hedgehogs, desiredColor, moves);
+
+        return moves;
+    }
+
+    private static int HandleColorComparison(int[] hedgehogs, int desiredColor, int moves)
+    {
+        if (hedgehogs[(desiredColor + 1) % 3] > hedgehogs[(desiredColor + 2) % 3])
+        {
+            moves += hedgehogs[(desiredColor + 2) % 3];
+            hedgehogs[desiredColor] += 2 * hedgehogs[(desiredColor + 2) % 3];
+            hedgehogs[(desiredColor + 1) % 3] -= hedgehogs[(desiredColor + 2) % 3];
+            hedgehogs[(desiredColor + 2) % 3] -= hedgehogs[(desiredColor + 2) % 3];
+
+            if (hedgehogs[(desiredColor + 2) % 2] > hedgehogs[desiredColor])
+            {
+                return -1;
+            }
+            else
+            {
+                moves += hedgehogs[(desiredColor + 1) % 3];
+            }
+
+        }
+        if (hedgehogs[(desiredColor + 1) % 3] < hedgehogs[(desiredColor + 2) % 3])
+        {
+            moves += hedgehogs[(desiredColor + 1) % 3];
+            hedgehogs[desiredColor] += 2 * hedgehogs[(desiredColor + 1) % 3];
+            hedgehogs[(desiredColor + 2) % 3] -= hedgehogs[(desiredColor + 1) % 3];
+            hedgehogs[(desiredColor + 1) % 3] -= hedgehogs[(desiredColor + 1) % 3];
+
+            if (hedgehogs[(desiredColor + 1) % 3] > hedgehogs[(desiredColor)])
+            {
+                return -1;
+            }
+            else
+            {
+                moves += hedgehogs[(desiredColor + 2) % 3];
+            }
+        }
+        return moves;
+    }
+
+}
